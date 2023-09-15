cfd8f7178e05a09975c275f05ec4ee6cb205d040
Dezheng Li <91477008+DezhengLee@users.noreply.github.com>
2023-09-15 20:17:24 +0200

    Update mailTool.py

--- mailTool.py
+++ mailTool.py
@@ -6,19 +6,19 @@ from email.utils import formataddr
 def mail(mailText: str, mailSubject: str, mailSender: str, mailVPW: str) -> bool:
     ret = True
     try:
-        my_sender = mailSender  # 发件人邮箱账号
-        my_pass = mailVPW  # 发件人邮箱密码
-        my_user = mailSender  # 收件人邮箱账号，我这边发送给自己
+        my_sender = mailSender  # Sender's mail address
+        my_pass = mailVPW  # Sender's mail password
+        my_user = mailSender  # Reciever's mail address
         msg = MIMEText(mailText, 'plain', 'utf-8')
-        msg['From'] = formataddr(["图书馆自动预约系统", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
-        msg['To'] = formataddr(["Me", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
-        msg['Subject'] = mailSubject  # 邮件的主题，也可以说是标题
+        msg['From'] = formataddr(["图书馆自动预约系统", my_sender])
+        msg['To'] = formataddr(["Me", my_user]) 
+        msg['Subject'] = mailSubject  # Mail title
 
-        server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是465
-        server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
-        server.sendmail(my_sender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
-        server.quit()  # 关闭连接
-    except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+        server = smtplib.SMTP_SSL("smtp.qq.com", 465)  
+        server.login(my_sender, my_pass) 
+        server.sendmail(my_sender, [my_user, ], msg.as_string())  
+        server.quit()  
+    except Exception:  
         ret = False
     return ret
 
